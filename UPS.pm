fa9ef78f074e6c55051cc87f8c51e10c09ff0f74
Mark Solomon <msolomon@seva.net>
1998-02-28 06:33:53 +0000

    Import of MSOLOMON/UPS.pm-1.07 from CPAN.
    gitpan-cpan-distribution: UPS
    gitpan-cpan-version:      1.07
    gitpan-cpan-path:         MSOLOMON/UPS.pm-1.07.tar.gz
    gitpan-cpan-author:       MSOLOMON
    gitpan-cpan-maturity:     released

--- UPS.pm
+++ UPS.pm
@@ -12,7 +12,7 @@ require 5.003;
 BEGIN {
 	# set the version for version checking
         # if using RCS/CVS, this may be preferred
-        $VERSION = do { my @r = (q$Revision: 1.6 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r }; # must be all one line, for MakeMaker
+        $VERSION = do { my @r = (q$Revision: 1.7 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r }; # must be all one line, for MakeMaker
 }
 
 sub main::getUPS {
@@ -51,13 +51,78 @@ sub main::getUPS {
 	}
 }
 
+
+#
+#	UPStrack sub added 2/27/1998
+#
+
+
+sub main::UPStrack {
+	my ($tracking_number) = @_;
+	my %retValue = {};
+	$tracking_number || die "No number to track!";
+	#my $h = `cat output`;
+	#my $h = `cat err`;
+	$h = get("http://wwwapps.ups.com/tracking/tracking.cgi?tracknum=$tracking_number");
+
+	$h =~ s#<.*?>##gi;	# Remove html tags
+	$h =~ s#\&nbsp;##gi;	# Remove '&nbsp' separators
+
+	# Get top and bottom of data
+	#
+	my $scan_sep = 'Scanning Information';
+	my $notice_sep = 'Notice';
+	my ($head, $scan_info) = split($scan_sep,$h);	# Separate scanning info
+	my ($head,$notice) = split($notice_sep,$head);	# Separate notice
+
+	# Check if there's an error
+	#
+	$error_key = 'Unable to track';
+	if ($head =~ /($error_key\s.*)\s\s\s\s/ ) {
+		$error = $1;
+		$error =~ s/\s+$/ /g;
+		$retValue{'error'} = $error;
+		return %retValue;
+	}
+
+	# Assign Notice
+	$notice =~ s/[\r\n]//g;
+	$retValue{'Notice'} = $notice;
+
+	# Prepare 'scanning info' for hash
+	#
+	($scan_info) = split('\n\n',$scan_info);	# Remove bottom junk
+	$scan_info =~ s/^\n//;				# Remove first blank
+	my @scan_info = split('\n',$scan_info);		# Break into lines
+	my %scan_info;
+	# Iterate and make hash of scan code and status
+	for ($i=0 ; $i < $#scan_info ; $i = $i + 2) {
+		# print "$i: \$scan_info{ \$scan_info[ $i ] } = \$scan_info[ $i+1 ]\n";
+		$scan_info{$scan_info[$i]} = $scan_info[$i+1];
+		# $i++;
+	}
+	$retValue{'scan'} = \%scan_info;		# Assign reference to return value
+
+	my @html = split('\n',$head);			# Start work on line
+	my $line;					#   separated above
+	foreach $line (@html) {
+		next if ! $line;
+		my ($key,$value) = ($line =~ /(.*?):(.*)/); # Split on ':'
+		$retValue{$key} = $value if $value;	# Assign to main hash
+		# print "$0: $key : $value\n" if $value;
+	}
+	return %retValue;
+}
+
+
+
 END {}
 
 __END__
 
 =head1 NAME
 
-	Business::UPS - A UPS Shipping Cost Module
+	Business::UPS - A UPS Interface Module
 
 =head1 SYNOPSIS
 
@@ -67,6 +132,11 @@ __END__
     $error and die "ERROR: $error\n";
     print "Shipping is \$$shipping\n";
     print "UPS Zone is $ups_zone\n";
+
+    %t = UPStrack("z10192ixj29j39");
+    *scan = $t{scan};
+    $t{error} and die "ERROR: $t{error};
+    print "This package is $t{'Current Status'}\n"; # 'Delivered' or 'In-transit'
 
 =head1 DESCRIPTION
 
@@ -89,7 +159,7 @@ LWP Module
 
 =back 4
 
-=head1 ARGUMENTS
+=head1 ARGUMENTS for getUPS()
 
 	Call the subroutine with the following values:
 		1. Product code
@@ -123,6 +193,12 @@ LWP Module
 		  3DS		3 Day Select
 		  GNDCOM	Ground Commercial
 		  GNDRES	Ground Residential
+		  XPR		Worldwide Express
+		  XDM		Worldwide Express Plus
+		  XPRL		Worldwide Express Letter
+		  XDML		Worldwide Express Plus Letter
+		  XPD		Worldwide Expedited
+
 
 	In an HTML "option" input it might look like this:
 
@@ -155,7 +231,15 @@ LWP Module
 
 		Weight of the package in pounds
 
+
+=head1 ARGUMENTS for UPStrack()
+
+	The tracking number.
+
+
 =head1 RETURN VALUES
+
+=item getUPS()
 
 	The raw http get() returns a list with the following values:
 
@@ -176,7 +260,30 @@ LWP Module
 
 	If anyone wants these available for some reason, let me know.
 
+=item UPStrack()
+	
+	The hash that's returned is like the following:
+		'Delivered on' 	=> '1-22-1998 at 2:58 PM'
+		'Notice' 	=> 'UPS authorizes you to use UPS...'
+		'Received by'	=> 'DR PORCH'
+		'Addressed to'	=> 'NEWPORT NEWS, VA US'
+		'scan'		=>  HASH(0x146e0c) (more later...)
+		'Current Status'=> 'Delivered'
+		'Delivered to'	=> 'RESIDENTIAL'
+		'Sent on'	=> '1-20-1998'
+		'UPS Service'	=> '2ND DAY AIR'
+		'Tracking Number' => '1ZX29W29025xxxxxx'
+
+	Notice the %hash{scan} is a reference to another hash for
+	the scanning information and is like the following:
+		'1-22-19982:58 PMNEWPORT NEWS-OYSTER, VA US' => 'DELIVERED'
+		'1-21-199811:37 PMRICHMOND, VA US'	     => 'LOCATION SCAN'
+		'2:05 PMPHILA AIR HUB, PA US' 		     => 'LOCATION SCAN'
+		'1-20-199811:35 PMPHILA AIR HUB, PA US'	     => 'LOCATION SCAN'
+
 =head1 EXAMPLE
+
+=item getUPS()
 
 	To retreive the shipping of a 'Ground Commercial' Package 
 	weighing 25lbs. sent from 23001 to 24002 this package would 
@@ -190,6 +297,33 @@ LWP Module
 	  $error and die "ERROR: $error\n";
 	  print "Shipping is \$$shipping\n";
 	  print "UPS Zone is $ups_zone\n";
+
+=item UPStrack()
+
+	#!/usr/local/bin/perl
+
+	use Business:UPS;
+
+	%t = UPStrack("z10192ixj29j39");
+	*scan = $t{scan};
+	$t{error} and die "ERROR: $t{error};
+	
+	print "This package is $t{'Current Status'}\n"; # 'Delivered' or 'In-transit'
+	print "More info:\n";
+	foreach $key (keys %t) {
+		print "KEY: $key = $t{$key}\n";
+	}
+	foreach $key (keys %scan) {
+		print "SCANKEY $key = $scan{$key}\n";
+	}
+
+
+=head1 BUGS
+
+	* I don't like the way the scanning information is returned.  It's not
+	in chronological order, and the way the html tags are removed, the city
+	and am/pm is not spaced.
+
 
 =head1 AUTHOR
 
